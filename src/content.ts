88d8c366eab44097d38faa54284a935b3610d1f5
Gabriel Del <gabrieldelc.barros@gmail.com>
2024-05-14 12:22:01 -0300

    put variables outside function main

--- src/content.ts
+++ src/content.ts
@@ -2,44 +2,46 @@ import {Message} from './types'
 
 console.info('Simple Autoscroll Loaded')
 let isLooping = false
+let intCB: number = -1
+let scrollDuration: number | null = null
+let scrollPixels: number | null = null
+let scrollLoop: boolean | null = null
 
 const element = window.location.origin === 'https://docs.google.com'
   ? document.querySelector('.kix-appview-editor')
   : null
 
+function startAutoscroll(scrollDuration: number, scrollPixels: number, loop: boolean){
+  if (intCB >= 0) clearInterval(intCB)
+  intCB = setInterval(() => {
+    const elements = [element, document?.body, document?.body?.parentNode].filter(Boolean) as Element[]
+    for (const element of elements) {
+      const percentage = element.scrollTop / (element.scrollHeight - element.clientHeight)
+      const isDone = percentage > 0.99
+      const scrollTop = element.scrollTop
+      if (isLooping) {
+        if (percentage < 0.01) isLooping = false
+      } else {
+        element.scroll(0, scrollTop + scrollPixels!)
+      }
+      const delta = element.scrollTop - scrollTop
+      if (isDone && loop) {
+        // Some websites slow down scrolling, causing the looping function to potentially break as it takes too long to reach the top. To fix it
+        isLooping = true
+        element.scroll({top: 0, behavior: 'auto'})
+      }
+      if (delta) break
+    }
+  }, scrollDuration)
+}
+
 async function main() {
-  if (globalThis.chrome) {
-    let intCB: number = -1
-    let scrollDuration: number | null = null
-    let scrollPixels: number | null = null
-    let scrollLoop: boolean | null = null
+
     const stopAutoscroll = () => {
       clearInterval(intCB)
       intCB = -1
     }
-    const startAutoscroll = (scrollDuration: number, scrollPixels: number, loop: boolean) => {
-      if (intCB >= 0) clearInterval(intCB)
-      intCB = setInterval(() => {
-        const elements = [element, document?.body, document?.body?.parentNode].filter(Boolean) as Element[]
-        for (const element of elements) {
-          const percentage = element.scrollTop / (element.scrollHeight - element.clientHeight)
-          const isDone = percentage > 0.99
-          const scrollTop = element.scrollTop
-          if (isLooping) {
-            if (percentage < 0.01) isLooping = false
-          } else {
-            element.scroll(0, scrollTop + scrollPixels!)
-          }
-          const delta = element.scrollTop - scrollTop
-          if (isDone && loop) {
-            // Some websites slow down scrolling, causing the looping function to potentially break as it takes too long to reach the top. To fix it
-            isLooping = true
-            element.scroll({top: 0, behavior: 'auto'})
-          }
-          if (delta) break
-        }
-      }, scrollDuration)
-    }
+
 
     chrome.runtime.onMessage.addListener(message => {
       if (message) {
@@ -62,6 +64,5 @@ async function main() {
       }
     })
   }
-}
 
 main()
